bcc1135a028ec83b33788b32cd0b725efc45acca
Madhura Bhave <mbhave@pivotal.io>
2017-11-16 17:10:30 -0800

    Initial commit

---
+++ src/main/java/com/example/tddwebfluxsample/TddWebfluxSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.tddwebfluxsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddWebfluxSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddWebfluxSampleApplication.class, args);
+	}
+}
