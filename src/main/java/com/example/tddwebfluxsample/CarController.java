57d3f222578b6d5bb073112b475a07aee27335d0
Madhura Bhave <mbhave@pivotal.io>
2017-11-16 17:12:27 -0800

    Implement controller

--- src/main/java/com/example/tddwebfluxsample/CarController.java
+++ src/main/java/com/example/tddwebfluxsample/CarController.java
@@ -1,6 +1,28 @@
 package com.example.tddwebfluxsample;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CarController {
 
+    private final CarRepository carRepository;
 
+    public CarController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    @GetMapping("/cars")
+    public Flux<Car> getCars () {
+        return this.carRepository.findAll();
+    }
+
+    @GetMapping("/cars/{name}")
+    public Mono<Car> getCar (@PathVariable String name) {
+        return this.carRepository.findByName(name);
+    }
 }
